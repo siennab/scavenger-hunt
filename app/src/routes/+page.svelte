dc6ae0cb8c7efcf76886b73b87c27cb5331926fd
siennab <siennabast@gmail.com>
2023-12-26 14:47:35 -0600

    Build to root

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,13 +1,11 @@
-<h1>Forgotten <br /> Speakeasy</h1>
+<h1>Forgotten <br /> &nbsp; &nbsp; &nbsp; Speakeasy</h1>
 <div class="container-inner">
-    Our group has come upon an abandoned speakeasy! Search for treasures to win a prize. 
-
-    <h4>Rules</h4>
-    <ul>
-        <li>No need to open drawers or cabinet doors, the treasure is in plain sight.</li>
-        <li>Find the least noticable treasure to win more points. You will see a small sticker next to valid items.</li>
-
-    </ul>
+    <p>
+        Welcome, intrepid explorers, to a once lively speakeasy turned silent sanctuary. The challenge? Find unique treasures hidden within these walls, but here's the twist – you must be the sole discoverer of each prize.
+        
+     <p>   Amidst the echoes of a bygone era, your mission is clear. Navigate the shadows, sift through forgotten corners, and uncover treasures others may overlook. It's a race against your fellow seekers, a test of cunning and observation.
+        
+      <p> No ordinary scavenger hunt, this clandestine quest demands subtlety and strategy. The artifacts are numerous, but victory lies in singularity. Will you unravel the secrets left behind in this clandestine haven? The stage is set; let the silent hunt begin.
     <br />
     <center>
         <a href="/user" class="button">Get Started!</a>
--- app/src/routes/user/+page.svelte
+++ app/src/routes/user/+page.svelte
@@ -1,14 +1,23 @@
 
 <center>
     <h2>Your Name</h2>
-    <input bind:value={name}  />
+    <input type="text" bind:value={name}  />
     <button class="button" onclick="{submitName(name)}"> Next</button>
 </center>
 
 <script>
+    import { onMount } from "svelte";
+
     let name = '';
 
     let submitName = (name) => {
        // window.localStorage.setItem('sh-playerName', name );
     }
+
+    onMount(() => {
+        let playerName = window.localStorage.getItem('sh-playerName');
+        if (playerName) {
+            name = playerName;
+        }
+    });
 </script>
